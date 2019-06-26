a921c9f780fecbd840d811f9d7d267317422e624
Shawn Johnson <shawnj@mobileapphero.com>
2019-06-26 03:55:08 -0700

    updated project

--- debugger_example/debugger_example/ViewController.swift
+++ debugger_example/debugger_example/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Let names = ["Bruce","Robin","Alfred"]
+        let names = ["Bruce","Will","Alfred"]
         
         for name in names {
             
